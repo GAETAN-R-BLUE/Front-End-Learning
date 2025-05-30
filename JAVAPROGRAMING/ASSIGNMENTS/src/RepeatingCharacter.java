aa3ced1985702bc7986395954c4a6f82d67262c6
GAETAN-R-BLUE <gaetanrodrigue_98@yahoo.com>
2025-05-30 09:51:18 -0400

    Cleaning up

--- JAVAPROGRAMING/ASSIGNMENTS/src/RepeatingCharacter.java
+++
@@ -1,6 +0,0 @@
-public class RepeatingCharacter {
-    public static void main(String[] args) {
-        System.out.println("Enter your word or sentence here");
-        
-    }
-}
--- JAVAPROGRAMING/ASSIGNMENTS/src/WordCount.java
+++
@@ -1,8 +0,0 @@
-// This program counts how many words are in a sentence
-import java.util.Scanner;
-public class WordCount {
-    public static void main(String[] args) {
-        System.out.println("Type your full sentence");
-        System.out.println("your sentence has " + new Scanner(System.in).nextLine().split(" ").length + " words");
-    }
-}
